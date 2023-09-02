7f31457ef2192f4e507db5796c362086870564c6
Naval Gupte <naval@hpcscale.com>
2023-09-02 12:33:31 -0700

    Structured Binding Demo in C++ 17 - Unpacking Structs

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <tuple>
 #include <utility>
+#include <vector>
 
 std::pair<int, int> divide(int dividend, int divisor) {
     return std::make_pair(dividend/divisor, dividend%divisor);
@@ -10,8 +11,19 @@ std::pair<int, int> divide(int dividend, int divisor) {
 std::tuple<std::string, std::chrono::system_clock::time_point, int>
     stock_info(const std::string& symbol) {
 
-        return std::make_tuple("ABC Corporation", std::chrono::system_clock::now(), 103);
+    return std::make_tuple("ABC Corporation", std::chrono::system_clock::now(), 103);
+}
+
+struct employee {
+    unsigned id;
+    std::string name;
+    std::string role;
+    unsigned salary;
+
+    employee(unsigned a_id, std::string a_name, std::string a_role, unsigned a_salary): 
+        id(a_id), name(a_name), role(a_role), salary(a_salary) {
     }
+};
 
 int main(int argc, char** argv) {
     // Unpacking Pair
@@ -21,6 +33,19 @@ int main(int argc, char** argv) {
     // Unpacking Tuple
     const auto [name, time, price] = stock_info("abc");
     std::cout << "Stock Price Of " << name << " Is " << price << ".\n\n";
+
+    // Create Vector Of Employees
+    std::vector<employee> employees;
+
+    employee e1(1, "John Doe", "CEO", 1000000);
+    employees.push_back(e1);
+    employee e2(2, "Jane Doe", "CFO", 1000000);
+    employees.push_back(e2);
+
+    // Unpacking Struct
+    for(const auto &[e_id,  e_name, e_role, e_salary]: employees) {
+        std::cout << "Name: " << e_name << " Role: " << e_role << " Salary: " << e_salary << std::endl;
+    }
 	
 	return 0;
 }
